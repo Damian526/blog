307f398e453454fb54ca0c0e4106c7d3b5e8b31f
Damian526 <damianzieba999@gmail.com>
2025-05-26 23:53:30 +0200

    eleminate fuoc

--- src/lib/StyledComponentsRegistry.tsx
+++ src/lib/StyledComponentsRegistry.tsx
@@ -1,11 +1,29 @@
 'use client';
 
-import { StyleSheetManager } from 'styled-components';
+import React, { useState } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default function StyledComponentsRegistry({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <StyleSheetManager>{children}</StyleSheetManager>;
+  // Only executed on client - ServerStyleSheet is for server-side rendering
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    // @ts-ignore - `styledComponentsStyleSheet.instance` is a private property but necessary here
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') return <>{children}</>;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children}
+    </StyleSheetManager>
+  );
 }
